ebd75d87b36cd120b009e8d9231ed16ba27c719b
djrussell <djrussell@kingston.ac.uk>
2007-06-11 09:36:13 +0000

    dan1patch

--- attempt5.hs
+++ attempt5.hs
@@ -140,19 +140,31 @@ perConstrainedness (Range t l u) =
 -- 10.3 Encoding as a non-negative-binary-integer
 -- 10.3.6
 minOctets :: Int -> [Int]
-minOctets =  
+minOctets =
    reverse . flip (curry (unfoldr (uncurry g))) 8 where
       g 0 0 = Nothing
       g 0 p = Just (0,(0,p-1))
       g n 0 = Just (n `mod` 2,(n `div` 2,7))
       g n p = Just (n `mod` 2,(n `div` 2,p-1))
 
-minBits = 
+{-
+    A new version of this function which avoids curry/uncurry is
+    implemented below.
+
+minBits =
    ((.).(.)) (reverse . (map fst)) (curry (unfoldr (uncurry h)))
       where
          h _ 0 = Nothing
          h 0 w = Just ((0,w `mod` 2),(0,w `div` 2))
          h n w = Just ((n `mod` 2, w `mod` 2),(n `div` 2,w `div` 2))
+-}
+
+minBits
+    = (reverse . unfoldr h)
+      where
+        h (_,0) = Nothing
+        h (0,w) = Just (0, (0, w `div` 2))
+        h (n,w) = Just (n `mod` 2, (n `div` 2, w `div` 2))
 
 -- 10.9 General rules for encoding a length determinant
 -- 10.9.4
@@ -160,9 +172,9 @@ lengthDeterminant n (Constrained (Just lb) (Just ub))
 -- 10.9.4.1
    | ub < 64*(2^10) = minOctets n
 -- 10.9.4.2, 10.9.3.5, 10.9.3.6 Note not very efficient since we know log2 128 = 7
-   | n <= 127       = 0:(minBits n 127)
+   | n <= 127       = 0:(minBits (n, 127))
 -- 10.9.3.7 Note not very efficient since we know log2 16*(2^10) = 14
-   | n < 16*(2^10)  = 1:0:(minBits n (16*(2^10)-1))
+   | n < 16*(2^10)  = 1:0:(minBits (n, (16*(2^10)-1)))
 -- 10.9.3.8
    where
       range = (ub - lb + 1)
@@ -174,10 +186,10 @@ encode x t =
       Constrained (Just lb) (Just ub) ->
          let range = ub - lb + 1 in
             if range <= 1
-               -- 10.5.4 
+               -- 10.5.4
                then []
                -- 10.5.6 and 10.3 Encoding as a non-negative-binary-integer
-               else minBits (x-lb) range
+               else minBits ((x-lb),range)
       -- 12.2.3, 10.7 Encoding of a semi-constrained whole number,
       -- 10.3 Encoding as a non-negative-binary-integer, 12.2.6, 10.9 and 12.2.6 (b)
       Constrained (Just lb) Nothing ->
@@ -197,7 +209,7 @@ groupBy n =
 insertLengths = unfoldr k
 
 k [] = Nothing
-k (x:xs) 
+k (x:xs)
    | l == n && lm >= l1b = Just (ws,xs)
    | l == 1 && lm <  l1b = Just (us,[])
    | otherwise           = Just (vs,[])
@@ -205,21 +217,21 @@ k (x:xs)
       l   = length x
       m   = x!!(l-1)
       lm  = length m
-      ws  = (1:1:(minBits l w6)):(concat x)
+      ws  = (1:1:(minBits (l,w6))):(concat x)
       us  = ld (length m) ++ m
       vs  = if lm >= l1b then
-               (1:1:(minBits l w6)):(concat x ++ ld 0)
+               (1:1:(minBits (l,w6))):(concat x ++ ld 0)
             else
-               ((1:1:(minBits (l-1) w6)):(concat (take (l-1) x)) ++ ld (length m) ++ m)
+               ((1:1:(minBits ((l-1), w6))):(concat (take (l-1) x)) ++ ld (length m) ++ m)
       n   = 4
       w6  = 2^6 - 1
       l1b = 16*(2^10)
 
-ld n 
+ld n
 -- 10.9.4.2, 10.9.3.5, 10.9.3.6 Note not very efficient since we know log2 128 = 7
-   | n <= 127       = [0:(minBits n 127)]
+   | n <= 127       = [0:(minBits (n, 127))]
 -- 10.9.3.7 Note not very efficient since we know log2 16*(2^10) = 14
-   | n < 16*(2^10)  = [1:0:(minBits n (16*(2^10)-1))]
+   | n < 16*(2^10)  = [1:0:(minBits (n, (16*(2^10)-1)))]
 -- Note there is no clause for >= 16*(2^10) as we have groupBy 16*(2^10)
 
 compress :: ConstrainedType a -> a -> [Int]
@@ -227,12 +239,18 @@ compress INTEGER x = encode x INTEGER
 compress r@(Range INTEGER l u) x = encode x (Range INTEGER l u)
 compress (SEQUENCE s) x = compressSeq s x
 
-compressSeq s x = concat (compressSeqAux [] [] s x)
+compressIntWithRange :: ConstrainedType Int -> Maybe Int -> Maybe Int -> Int -> [Int]
+compressIntWithRange INTEGER u l x = encode x (Range INTEGER u l)
+compressIntWithRange r@(Range t l u) m v x = 
+   compressIntWithRange t rl ru x where
+      (Constrained' rl ru) = (Constrained' l u) `mappend` (Constrained' m v)
+
+compressSeq = compressSeqAux [] []
 
 compressSeqAux :: [Int] -> [[Int]] -> Sequence a -> a -> [[Int]]
 compressSeqAux preamble body Nil _ = (reverse preamble):(reverse body)
 compressSeqAux preamble body (Cons a as) (x:*:xs) = compressSeqAux preamble ((compress a x):body) as xs
-compressSeqAux preamble body (Optional a as) (Nothing:*:xs) = 
+compressSeqAux preamble body (Optional a as) (Nothing:*:xs) =
    compressSeqAux (0:preamble) body as xs
 compressSeqAux preamble body (Optional a as) ((Just x):*:xs) =
    compressSeqAux (1:preamble) ((compress a x):body) as xs
@@ -240,7 +258,7 @@ compressSeqAux preamble body (Optional a as) ((Just x):*:xs) =
 perConstrainedness' :: Ord a => ConstrainedType a -> Constraint' a
 perConstrainedness' INTEGER = Constrained' Nothing Nothing
 {-
-perConstrainedness' (Includes t1 t2) = 
+perConstrainedness' (Includes t1 t2) =
    (perConstrainedness' t1) `mappend` (perConstrainedness' t2)
 -}
 perConstrainedness' (Range t l u) =
@@ -278,7 +296,7 @@ FooBaz {1 2 0 0 6 3} DEFINITIONS ::=
          SEQUENCE {
             first  T1 OPTIONAL,
             second T1 OPTIONAL
-         }      
+         }
    END
 -}
 
@@ -288,8 +306,11 @@ t3 = Includes t1 t1
 t4 = Range INTEGER (Just (-256)) Nothing
 t5 = SEQUENCE (Cons t4 (Cons t4 Nil))
 
+test0 = compress t1 27
+
 test1 = compress (SEQUENCE (Cons (SEQUENCE (Cons t1 Nil)) Nil)) ((27:*:Empty):*:Empty)
 test2 = compress (SEQUENCE (Cons t1 (Cons t1 Nil))) (29:*:(30:*:Empty))
+test20 = compress (SEQUENCE (Cons t1 (Cons t1 (Cons t1 Nil)))) (29:*:(30:*:(26:*:Empty)))
 test3 = compress (SEQUENCE (Optional t1 (Optional t1 Nil))) ((Just 29):*:((Just 30):*:Empty))
 petest2 = compress (SEQUENCE (Optional t1 (Optional t1 Nil)))
 test4 = petest2 ((Just 29):*:((Just 30):*:Empty))
