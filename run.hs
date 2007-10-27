31c47a3dc426b330d91fc42f7977c2885b58ac26
dominic.steinitz <dominic.steinitz@blueyonder.co.uk>
2007-10-27 06:48:08 +0000

    More automation of asn1c test harness.

--- run.hs
+++ run.hs
@@ -1,11 +1,22 @@
 import System.Process
 import System.Exit
 
-main = 
-   do pH <- runCommand "gcc -I. -o test *.c"
-      ec <- waitForProcess pH
-      case ec of
+runTest f =
+   runCommands [
+      ("asn1c -gen-PER " ++ f, "Failure in asn1c"),
+      ("mv converter-sample.c converter-sample.c.old", "Failure in mv of converter-sample.c"),
+      ("gcc -I. -o test *.c",  "Failure in gcc")
+   ]
+
+runCommands [] =
+   return "Success"
+runCommands ((c,e):xs) =
+   do h <- runCommand c
+      d <- waitForProcess h 
+      case d of
          ExitSuccess ->
-            putStrLn "Success"
+            runCommands xs 
          ExitFailure n ->
-            putStrLn ("Failure " ++ show n)
+            return (e ++ ": " ++ show n)
+
+main = runTest "../Segment.asn1"
