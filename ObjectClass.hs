a69b173c8df926518dea6f864628b7e86ea0f453
dominic.steinitz <dominic.steinitz@blueyonder.co.uk>
2008-04-06 17:22:58 +0000

    First attempt at Object Classes.

---
+++ ObjectClass.hs
@@ -0,0 +1,81 @@
+module ObjectClass where
+
+import ConstrainedType hiding (Cons)
+
+data ObjClass :: * -> * where
+   Singleton :: ObjClassComponent a -> ObjClass a
+   Cons      :: ObjClassComponent a -> ObjClass l -> ObjClass (a:*:l)
+   
+type FieldName = String
+
+data ObjClassComponent :: * -> * where
+   OCType :: FieldName -> ObjClassComponent a
+   OCFixedTypeValue :: FieldName -> ASNType a -> ObjClassComponent a
+   OCVariableTypeValue :: FieldName -> ObjClassComponent a -> ObjClassComponent a
+   OCFixedTypeValueSet :: FieldName -> ASNType a -> ObjClassComponent a
+   OCVariableTypeValueSet :: FieldName -> ObjClassComponent a -> ObjClassComponent a
+   OCInformationObject :: FieldName -> ObjClass a -> ObjClassComponent a
+
+otherFunction1 = OCType "ArgumentType"
+
+otherFunction2 = OCFixedTypeValue "code" INTEGER
+
+otherFunction3 = OCType "ResultType"
+
+otherFunction4 = OCVariableTypeValue "result-if-error" otherFunction3
+
+otherFunction5 = OCFixedTypeValueSet "Alphabet" IA5STRING
+
+otherFunction6 = OCVariableTypeValueSet "Suppoerted Arguments" otherFunction1
+
+foo = Singleton (OCInformationObject "associatedFunction" foo)
+
+baz x = (Cons otherFunction2 (Singleton (OCInformationObject "associatedFunction" x)))
+
+
+-- From Mark Jones
+-- data Mu f = In (f (Mu f))
+
+-- (* -> * -> *) -> * -> (* -> *)
+data Sigma f a = In (f a (Sigma f a))
+
+type Foobar = Sigma (:*:) Integer
+
+fee = In ((3::Integer) :*: fee)
+
+fi = In (otherFunction2 :*: fi)
+
+-- (* -> * -> *) -> * -> * -> (* -> * -> *) -> (* -> *)
+data Phi f a b g = Jn (g (f a b) (Phi f a b g))
+
+faa = Jn ((:*:) ((:*:) (3::Integer) ('a'::Char)) faa)
+
+data Psi a f b g = Kn (g (f a b) (Psi a f b g))
+
+fuu = Kn ((:*:) ((:*:) (3::Integer) ('a'::Char)) fuu)
+
+fu = Kn ((:*:) ((:*:) otherFunction3 otherFunction4) fu)
+
+fu' = Kn ((otherFunction3 :*: otherFunction4) :*: fu')
+
+fu'' = Kn (otherFunction3 :*: otherFunction4 :*: fu'')
+
+data Xi f a b c = Ln (f (f (f a b) c) (Xi f a b c))
+
+goo = Ln ('a' :*: 'b' :*: 'c' :*: goo)
+
+gee = Ln (otherFunction2 :*: otherFunction3 :*: otherFunction4 :*: gee)
+
+f :: Xi f (ObjClassComponent a) (ObjClassComponent b) (ObjClassComponent c) -> a :*: b :*: c -> String
+f = undefined
+
+g = f gee (2 :*: IA5STRING :*: 0)
+
+data Alpha a b c = Mn ((:*:) ((:*:) ((:*:) a b) c) (Alpha a b c))
+
+h' :: Alpha (ObjClassComponent a) (ObjClassComponent b) (ObjClassComponent c) -> a :*: b :*: c -> String
+h' = undefined
+
+gee' = Mn (otherFunction2 :*: otherFunction3 :*: otherFunction4 :*: gee')
+
+i = h' gee' (2 :*: IA5STRING :*: 0)
