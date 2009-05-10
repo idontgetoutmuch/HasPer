32ac78ff9e7b8cc2fa150e72a39b15f1a9aeff52
dominic.steinitz <dominic.steinitz@blueyonder.co.uk>
2009-05-10 19:17:58 +0000

    Improve Setup to check for gcc or lcc depending whether we are on linux or windows.

--- Setup.hs
+++ Setup.hs
@@ -1,4 +1,6 @@
 module Main where
+
+import System.Info
 import Distribution.Simple
 import Distribution.Simple.Program
 import Distribution.Simple.Setup
@@ -9,23 +11,30 @@ import Distribution.Simple.Utils
 
 main :: IO ()
 main = do 
-  let foo = simpleProgram "pdflatex"
   defaultMainWithHooks perHooks
 
-perHooks = 
+cCompilerName =
+   case os of
+      "mingw32" -> "gcc"
+      _         -> "lcc"
+
+perHooks =
    simpleUserHooks {
          hookedPrograms = [
               simpleProgram "pdflatex"
             , simpleProgram "asn1c"
+            , simpleProgram cCompilerName
           ]
       , postConf = perPostConf
    }
 
 perPostConf :: Args -> ConfigFlags -> PackageDescription  -> LocalBuildInfo -> IO ()
 perPostConf a cfs pd lbi =
-   do let v= fromFlagOrDefault normal (configVerbosity cfs)
-          mPdf = lookupProgram (simpleProgram "pdflatex") (withPrograms lbi)
+   do let v      = fromFlagOrDefault normal (configVerbosity cfs)
+          mPdf   = lookupProgram (simpleProgram "pdflatex") (withPrograms lbi)
           mAsn1c = lookupProgram (simpleProgram "asn1c") (withPrograms lbi)
+          mC     = lookupProgram (simpleProgram cCompilerName) (withPrograms lbi)
+      error "Foo"
       case mPdf of
          Nothing -> 
             warn v "Full documentation cannot be built without pdflatex" >> return ()
@@ -36,6 +45,10 @@ perPostConf a cfs pd lbi =
             warn v "Full inter-operability testing cannot be performed without asn1c" >> return ()
          Just _ ->
             return ()
-      
-
+      case mC of
+         Nothing -> 
+            warn v ("Full inter-operability testing cannot be performed without" ++ cCompilerName) >> return ()
+         Just cp -> do
+            info v (show cp)
+            return ()
        
