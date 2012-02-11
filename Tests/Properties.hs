2c6f020ccfe2cf7f48a3173bacb035820374a3ff
dominic <dominic@steinitz.org>
2012-02-11 20:19:03 +0000

    Remove old literate files (we can always get them back via 0.0.33). Add a unit test file to be tested via cabal.

---
+++ Tests/Properties.hs
@@ -0,0 +1,26 @@
+module Main (main) where
+
+import Test.HUnit hiding (Test)
+import Test.QuickCheck hiding ((.&.))
+import Test.Framework (Test, defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.Framework.Providers.HUnit
+
+import NewTestData
+import PERWriter
+import ASNTYPE
+import NewPretty
+
+roundTrip t v = runDecode t (runEncode t v)
+
+refIntTest1 = roundTrip rt1 v1 @?= v1
+
+refSeqTest1 = roundTrip rt3 v3 @?= v3
+
+tests :: [Test]
+tests =
+    [ testCase "Referenced unconstrained INTEGER" refIntTest1
+    , testCase "Referenced SEQUENCE" refSeqTest1
+    ]
+
+main = defaultMain tests
